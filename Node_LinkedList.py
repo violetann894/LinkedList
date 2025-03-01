77b2b5353557c6ee560d5c6ec81b55e982e902ba
violetann894 <rachelhuss2976@gmail.com>
2025-03-01 02:17:32 -0500

    Added comments

--- Node_LinkedList.py
+++ Node_LinkedList.py
@@ -1,30 +1,35 @@
 class Node:
 
-    def __init__(self, data, pointer):
+    """"""
+    def __init__(self, data):
         self.key = data
-        self.next = pointer
+        self.next = None
         self.prev = None
 
 
 class LinkedList:
 
+    """"""
     def __init__(self, head):
         self.head = head
 
+    """"""
     def listSearch(L, k):
         x = L.head
         while x is not None and x.key != k:
             x = x.next
         return x
 
+    """"""
     def listPrepend(L, k):
-        x = Node(k, None)
+        x = Node(k)
         x.next = L.head
         x.prev = None
         if L.head is not None:
             L.head.prev = x
         L.head = x
 
+    """"""
     def listInsert(L, x, y):
         x.next = y.next
         x.prev = y
@@ -32,6 +37,7 @@ class LinkedList:
             y.next.prev = x
         y.next = x
 
+    """"""
     def listDelete(L, x):
         if x.prev is not None:
             x.prev.next = x.next
@@ -40,15 +46,19 @@ class LinkedList:
             if x.next is not None:
                 x.next.prev = x.prev
 
+    """"""
     def printLinkedList(L):
         x = L.head
-        while x.next is not None:
+        while x is not None:
             print(x.key)
             x = x.next
 
 if __name__ == "__main__":
-    
-    linkList = LinkedList(Node(1, None))
+    head = Node(1)
+
+    linkList = LinkedList(head)
+    linkList.printLinkedList()
+    print()
     linkList.listPrepend(2)
     linkList.printLinkedList()
     print()
@@ -61,8 +71,11 @@ if __name__ == "__main__":
     linkList.listPrepend(5)
     linkList.printLinkedList()
     print()
- 
 
+    newNode = Node(6)
+
+    linkList.listInsert(linkList.listSearch(3), newNode)
+    linkList.printLinkedList()
     
 
     
