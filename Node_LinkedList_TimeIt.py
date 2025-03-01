e8c9bde17e21b390242e888419f1477a8cef1566
violetann894 <rachelhuss2976@gmail.com>
2025-03-01 02:40:28 -0500

    Initial Commit

---
+++ Node_LinkedList_TimeIt.py
@@ -0,0 +1,49 @@
+class Node:
+
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self, head):
+        self.head = head
+
+    def listSearch(L, k):
+        x = L.head
+        while x is not None and x.key != k:
+            x = x.next
+        return x
+
+    def listPrepend(L, k):
+        x = Node(k)
+        x.next = L.head
+        x.prev = None
+        if L.head is not None:
+            L.head.prev = x
+        L.head = x
+
+    def listInsert(L, x, y):
+        x.next = y.next
+        x.prev = y
+        if y.next is not None:
+            y.next.prev = x
+        y.next = x
+
+    def listDelete(L, x):
+        if x.prev is not None:
+            x.prev.next = x.next
+        else:
+            L.head = x.next
+            if x.next is not None:
+                x.next.prev = x.prev
+
+    def printLinkedList(L):
+        x = L.head
+        while x is not None:
+            print(x.key)
+            x = x.next
+
+
+if __name__ == "__main__":
