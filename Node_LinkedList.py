197416185fca41e4d86f81952d1c5e641cb38a97
violetann894 <rachelhuss2976@gmail.com>
2025-03-01 02:40:13 -0500

    Added comments

--- Node_LinkedList.py
+++ Node_LinkedList.py
@@ -1,26 +1,35 @@
 class Node:
 
-    """"""
-    def __init__(self, data):
-        self.key = data
+    """The constructor for the Node class creates a new Node object that holds information about the current state
+        of the Node.
+        key - The data that the Node needs to hold
+        next - The pointer to the next object, starts as none because nothing is connected to a newly created Node
+        """
+    def __init__(self, key):
+        self.key = key
         self.next = None
-        self.prev = None
 
 
 class LinkedList:
 
-    """"""
+    """The constructor for the LinkedList class creates a new LinkedList object.
+        head - The Node at the beginning of the list"""
     def __init__(self, head):
         self.head = head
 
-    """"""
+    """listSearch accepts a key value for a Node in the LinkedList and returns if the Node was found.
+        k - The key value for the desired Node
+        
+        Return x - The Node with the key value given, returns none if the Node was not found"""
     def listSearch(L, k):
         x = L.head
         while x is not None and x.key != k:
             x = x.next
         return x
 
-    """"""
+    """listPrepend accepts a key value for a Node, creates a new Node object and adds it to the beginning of the 
+        linked list.
+        k - The key value for the new head Node"""
     def listPrepend(L, k):
         x = Node(k)
         x.next = L.head
@@ -29,7 +38,9 @@ class LinkedList:
             L.head.prev = x
         L.head = x
 
-    """"""
+    """listInsert inserts a Node after a chosen Node.
+        x - The Node to be inserted after y
+        y - The Node that x will be inserted after"""
     def listInsert(L, x, y):
         x.next = y.next
         x.prev = y
@@ -37,7 +48,8 @@ class LinkedList:
             y.next.prev = x
         y.next = x
 
-    """"""
+    """listDelete deletes the desired node in the linked list.
+        x - The Node object to be deleted from the list"""
     def listDelete(L, x):
         if x.prev is not None:
             x.prev.next = x.next
@@ -46,7 +58,8 @@ class LinkedList:
             if x.next is not None:
                 x.next.prev = x.prev
 
-    """"""
+    """printLinkedList prints out the key values of the linked list, with the value at the top being the 
+        key value of the head of the list."""
     def printLinkedList(L):
         x = L.head
         while x is not None:
@@ -54,27 +67,30 @@ class LinkedList:
             x = x.next
 
 if __name__ == "__main__":
-    head = Node(1)
 
-    linkList = LinkedList(head)
-    linkList.printLinkedList()
-    print()
+    """Adding Nodes to the linked list"""
+    linkList = LinkedList(Node(1))
     linkList.listPrepend(2)
-    linkList.printLinkedList()
-    print()
     linkList.listPrepend(3)
-    linkList.printLinkedList()
-    print()
     linkList.listPrepend(4)
-    linkList.printLinkedList()
-    print()
     linkList.listPrepend(5)
+
+    """Testing the printLinkedList method"""
+    print("Printing out the linked list")
     linkList.printLinkedList()
     print()
 
     newNode = Node(6)
 
-    linkList.listInsert(linkList.listSearch(3), newNode)
+    """Testing the listInsert and the listSearch method"""
+    linkList.listInsert(newNode, linkList.listSearch(3))
+    print("Printing out the linked list after inserting a new node")
+    linkList.printLinkedList()
+    print()
+
+    """Testing the listDelete method"""
+    linkList.listDelete(linkList.listSearch(2))
+    print("Printing out the linked list after deleting a node")
     linkList.printLinkedList()
     
 
